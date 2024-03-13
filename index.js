fd132526a70966c3ebf9786af14dde21abd68a11
Bholanath <bholanathbarik9748@gmail.com>
2024-03-13 15:22:12 +0530

    Add morgon

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from "express";
-const app = express();
 import cors from 'cors';
-const PORT = 8000;
+import morgan from 'morgan'; // Import Morgan
 import "./db/conn.js";
 import router from "./router/routes.js";
-  app.use(cors());
-  app.use(express.json());
-  app.use(router);
-  app.listen(PORT, () => {
-    console.log(`Server running at: http://localhost:${PORT}/`);
-  });
+
+const app = express();
+const PORT = 8000;
+
+// Use Morgan logger
+app.use(morgan('dev'));
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});
